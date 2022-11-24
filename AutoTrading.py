73182fc4f76e8bf93e9dd47536d1252f95389cfa
tom17 <tom17@kakao.com>
2022-11-24 15:33:37 +0900

    logic modify

--- AutoTrading.py
+++ AutoTrading.py
@@ -334,7 +334,7 @@ if '__main__' == __name__:
                         time.sleep(1)
 
                 # 매시 30분 마다 프로세스 확인 메시지(슬랙)를 보낸다
-                if t_now.minute == 30 and 0 <= t_now.second <=10:
+                if t_now.minute == 30 and 0 <= t_now.second <=3:
                     my_stock = []
                     if t_now.hour > 12:
                         sell_stock_list = _check_profit()
